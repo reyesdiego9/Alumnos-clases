a427a002698738c5fc4a102522adf1f141ae88b1
Lenovo <Lenovo@LAPTOP-V7NOIE5K>
2019-08-13 09:32:10 -0600

    #1 Primer trabajo de orientacion a objetos

---
+++ src/clases/Clases.java
@@ -0,0 +1,34 @@
+/*
+Juan Diego Reyes Zepeda
+5090-18-5233
+*/
+package clases;
+
+
+class Alumnos{
+    private String nombre;
+    private String apellido;
+    private int n1,n2,n3,n4;
+    private double prom;
+    
+    Alumnos(String nombre, String apellido, int n1, int n2, int n3, int n4, double prom){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.n1 = n1;
+        this.n2 = n2;
+        this.n3 = n3;
+        this.n4 = n4;
+        this.prom = prom;
+    }
+    
+    
+}
+
+public class Clases {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
