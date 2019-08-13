bd447c25ba1ac8768beea6d0573d6baecc063993
Lenovo <Lenovo@LAPTOP-V7NOIE5K>
2019-08-13 09:48:06 -0600

    #1 Primer trabajo de orientacion a objetos

--- src/clases/Clases.java
+++ src/clases/Clases.java
@@ -21,6 +21,35 @@ class Alumnos{
         this.prom = prom;
     }
     
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    public void setN1(int n1){
+        this.n1 = n1;
+    }
+    public void setN2(int n2){
+        this.n2 = n2;
+    }
+    public void setN3(int n3){
+        this.n3 = n3;
+    }
+    public void setN4(int n4){
+        this.n4 = n4;
+    }
+    public void setProm(double prom){
+        this.prom = prom;
+    }
+    
+    public String getNombre(){return nombre;}
+    public String getApellido(){return apellido;}
+    public int getN1(){return n1;}
+    public int getN2(){return n2;}
+    public int getN3(){return n3;}
+    public int getN4(){return n4;}
+    public double getProm(){return prom;}
     
 }
 
